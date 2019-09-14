fbc0320ceb9b46fecd7e06eea499506d40cf4278
Davi de Carvalho <davi_carvalho96@hotmail.com>
2019-09-14 10:18:13 -0300

    Auto stash before merge of "master" and "origin/master"

--- src/login/vo/user-signin.vo.ts
+++ src/login/vo/user-signin.vo.ts
@@ -1,9 +1,14 @@
 import { ApiModelProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class UserSigninVO {
+  @IsString()
+  @IsNotEmpty()
   @ApiModelProperty({ required: true })
   email: string;
 
+  @IsString()
+  @IsNotEmpty()
   @ApiModelProperty({ required: true })
   senha: string;
 }
--- src/login/vo/user-signup.vo.ts
+++ src/login/vo/user-signup.vo.ts
@@ -1,17 +1,25 @@
 import { TelephoneVO } from './telephone.vo';
 import { Type } from 'class-transformer';
 import { ApiModelProperty } from '@nestjs/swagger';
+import { ArrayNotEmpty, IsArray, IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class UserSignupVO {
+  @IsString()
+  @IsNotEmpty()
   @ApiModelProperty({ required: true })
   nome: string;
 
+  @IsEmail()
   @ApiModelProperty({ required: true })
   email: string;
 
+  @IsString()
+  @IsNotEmpty()
   @ApiModelProperty({ required: true })
   senha: string;
 
+  @IsArray()
+  @ArrayNotEmpty()
   @Type(() => TelephoneVO)
   @ApiModelProperty({ isArray: true, type: TelephoneVO })
   telefones: TelephoneVO[];
